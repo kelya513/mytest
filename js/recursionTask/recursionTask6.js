9fc8bc3abe8e5c3260b0892e562e3a3de0eab71d
Nastya513 <kelnmiira@mail.ru>
2017-10-11 17:16:22 +0300

    fix name, add link

--- js/recursionTask/recursionTask6.js
+++ js/recursionTask/recursionTask6.js
@@ -2,19 +2,19 @@ function recursionTask6() {
     var n = +document.getElementById("task_6").value;
 
     if ( n > 0){
-        alert('Первые : ' + n + ' чисел Фибоначи: ' + FiboSeriesRec(n) );
+        alert('Первые : ' + n + ' чисел Фибоначи: ' + fiboSeriesRec(n) );
     } else {
         alert('Неккоректрые данные');
     }
 };
 
 //Последовательность чисел Фибоначчи имеет формулу Fn = Fn-1 + Fn-2
-function FiboSeriesRec(n) {
+function fiboSeriesRec(n) {
     if (n === 1) {
         return [0, 1]; // первые 2 элемент серии
     }
     else{
-        var list = FiboSeriesRec(n - 1); //идем в начало серии для создания массива и вычесления след. элементов
+        var list = fiboSeriesRec(n - 1); //идем в начало серии для создания массива и вычесления след. элементов
         list.push(list[list.length - 1] + list[list.length - 2]);
         return list;
     }

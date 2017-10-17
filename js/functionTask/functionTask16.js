bff5696bfb601cc3376083804f50508916aa8479
Nastya513 <kelnmiira@mail.ru>
2017-10-17 17:52:30 +0300

    решение  16й задачи в блоке функций

---
+++ js/functionTask/functionTask16.js
@@ -0,0 +1,19 @@
+function functionTask16() {
+    var str = document.getElementById("task_16").value;
+    if (str == ""){
+        alert('Введите строку');
+    } else {
+        alert('уникальные символы: ' + selectSymbol(str));
+    }
+};
+
+function selectSymbol(str) {
+    var result='';
+
+    for (var i = 0; i < str.length; i++){
+        if(result.indexOf(str.charAt(i))==-1){
+            result += str[i];
+        }
+    }
+    return result;
+};

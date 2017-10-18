b4e37e38c064d7027ecab05bc29b9dd09f24643e
Nastya513 <kelnmiira@mail.ru>
2017-10-18 14:37:53 +0300

    solution of 24th task from the function block

---
+++ js/functionTask/functionTask24.js
@@ -0,0 +1,27 @@
+'use strict';
+function functionTask24() {
+    var arr=[], size, arr_copy;
+    size = Math.floor(Math.random() * 9) + 2;
+
+    for (var i = 0; i < size; i++) {
+        var rand = Math.round(Math.random() * 15);
+        arr.push(rand)
+    }
+
+    arr_copy = arr.slice();
+
+    sortBubble(arr_copy);
+    alert('Исходный массив: '+ arr + '. Результат: ' + arr_copy);
+}
+
+function sortBubble(arr) {
+    for (var i = 0; i < arr.length; i++){
+        for (var j = 0; j < arr.length-i; j++){
+            if (arr[j]> arr[j+1]) {
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}

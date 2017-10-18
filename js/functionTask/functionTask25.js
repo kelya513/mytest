712c322d762fee1d3086d4c3a3319abf4dc40e63
Nastya513 <kelnmiira@mail.ru>
2017-10-18 14:53:58 +0300

    solution of 25th task from the function block

---
+++ js/functionTask/functionTask25.js
@@ -0,0 +1,15 @@
+'use strict';
+function functionTask25() {
+    var arr_Contry = ['Австралия', 'Германия', 'Соединенные Штаты Америки', 'Великобритания', 'Южная Корея' ];
+    alert('Исходный массив: '+ arr_Contry + '. ____Результат: ' + longNameCountry(arr_Contry));
+}
+
+function longNameCountry(arr) {
+    var result='';
+
+    for(var i = 0; i<arr.length; i++){
+        result = (result.length > arr[i].length) ? result : arr[i];
+    }
+
+    return result;
+}

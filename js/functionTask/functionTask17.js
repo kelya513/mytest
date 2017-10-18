4370b0f015dce158ee112c22f13c6a5b9dba4206
Nastya513 <kelnmiira@mail.ru>
2017-10-18 09:40:35 +0300

    fix

--- js/functionTask/functionTask17.js
+++ js/functionTask/functionTask17.js
@@ -3,11 +3,12 @@ function functionTask17() {
     if (str == ""){
         alert('Введите строку');
     } else {
+        cntSymbol17(str);
         alert('уникальные символы и их количество выведенно в консоле');
     }
 };
 
-function cntSymbol(str) {
+function cntSymbol17(str) {
     var resCntChar = {};
     str.split("").map(ch => resCntChar[ch] = isNaN(resCntChar[ch]) ? 1 : (resCntChar[ch] += 1)) ;
     console.log(resCntChar);

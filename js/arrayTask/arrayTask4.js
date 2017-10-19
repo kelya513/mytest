e39764ad1b864c2e3fab6dd401eb4d6813f13160
Nastya513 <kelnmiira@mail.ru>
2017-10-19 17:23:10 +0300

    4 task Array block

--- js/arrayTask/arrayTask4.js
+++ js/arrayTask/arrayTask4.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function arrayTask3() {
+function arrayTask4() {
     var arr=[], size;
     size = Math.floor(Math.random() * 9) + 5;
     for (var i = 0; i < size; i++) {
@@ -8,19 +8,20 @@ function arrayTask3() {
         arr.push(rand);
     }
 
-    var n = +document.getElementById("task_3").value;
+    var n = +document.getElementById("task_4").value;
     if (n >= 0){
         console.log(`исходный массив ${arr}`);
-        console.log(`копия n элементов массива ${copyPartArr(arr, n)}`);
+        console.log(`копия n элементов массива с конца ${copyPartArrRight(arr, n)}`);
     } else {
         alert('Введите число');
     }
 }
 
-function copyPartArr(arr,n) {
+function copyPartArrRight(arr,n) {
     n =  n>=0 ? n : 1;
-    if( Array.isArray(arr) ){
-        return arr.slice(0, n);
+    if( Array.isArray(arr)){
+        if (arr.length === 0){return 'array is empty'}
+        return arr.slice(-n);
     }else{
         return 'it`s not array';
     }

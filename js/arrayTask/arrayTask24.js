561057df0ad0cda1003d83c18421e8b01032c305
Nastya513 <kelnmiira@mail.ru>
2017-10-20 18:19:16 +0300

    24 task Array block

---
+++ js/arrayTask/arrayTask24.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function arrayTask24(){
+    var arr=[NaN, 0, 15, false, -22, '', undefined, 47, null] ;
+
+    console.log(`исходный массив ${arr}`);
+    console.log(`result: ${deleteFalseValue(arr)}`);
+}
+
+function deleteFalseValue(arr) {
+    var res =[];
+
+    for(let val of arr){
+        if (val) {
+            res.push(val);
+        }
+    }
+
+    return res;
+}
+

3015e23726bd0ffa3e8dd3de837df45769a94149
Nastya513 <kelnmiira@mail.ru>
2017-10-20 13:02:30 +0300

    15  task Array block

---
+++ js/arrayTask/arrayTask15.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function arrayTask15() {
+    const color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+    const o = ["th","st","nd","rd"];
+
+    console.log(`исходный массив color ${color}`);
+    // console.log(`исходный массив o ${o}`);
+
+    printOrdinalColor(color, o);
+
+}
+
+function printOrdinalColor(colorArr, oArr) {
+    for(let i=0; i < colorArr.length; i++){
+        if( i < 3 ){
+            console.log(`The ${i + oArr[i+1]} choice is ${colorArr[i]}`);
+        }else {
+            console.log(`The ${i + oArr[0]} choice is ${colorArr[i]}`);
+        }
+    }
+}

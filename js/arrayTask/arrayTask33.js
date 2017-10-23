6c27d2d4fc1ed8a6c4b49aa51b49107cc77a0eae
Nastya513 <kelnmiira@mail.ru>
2017-10-23 16:38:56 +0300

    29 task Array block

---
+++ js/arrayTask/arrayTask33.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function arrayTask33(){
+    var arr=[], size;
+    size = Math.floor(Math.random() * 9) + 5;
+    for (let i = 0; i < size; i++) {
+        let rand = Math.floor(- 5.5 + Math.random() * 26);
+        arr.push(rand);
+    }
+
+    console.log(`исходный массив ${arr}`);
+    delAllElemArray(arr);
+    console.log(`result удаление массива: ${arr} (array empty)`);
+}
+
+
+function delAllElemArray(arr) { // сама переменная остается, но значения елементов теряются.
+  //arr.length = 0;
+  arr.splice(0);
+}

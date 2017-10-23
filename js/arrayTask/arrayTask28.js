886a580a1e625c7f7e5c8d79cf5445abadc93de4
Nastya513 <kelnmiira@mail.ru>
2017-10-23 13:16:16 +0300

    25 task Array block

---
+++ js/arrayTask/arrayTask28.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function arrayTask28(){
+    var str1 = document.getElementById("task_28_1").value;
+    var str2 = document.getElementById("task_28_2").value;
+    if (str1 !== '' && str1 !== ''){
+        console.log(`результат: ${findLongSubstring([str2, str1])}`);
+    } else {
+        alert('Введите строки');
+    }
+}
+
+function findLongSubstring(arr) {
+    arr.sort((a,b)=>{return a.length < b.length});
+    var i = 0;
+
+    while(arr[0].charAt(i) == arr[1].charAt(i) && i < arr[0].length){
+        i++;
+    }
+
+    return arr[1].substring(0,i);
+
+}

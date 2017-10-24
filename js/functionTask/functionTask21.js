caf16c7c521516f68fe774243d5fc7659fb60207
Nastya513 <kelnmiira@mail.ru>
2017-10-24 10:30:39 +0300

    21  task functions block

--- js/functionTask/functionTask21.js
+++ js/functionTask/functionTask21.js
@@ -1,2 +1,48 @@
 'use strict';
-//Write a JavaScript function to get all possible subset with a fixed length (for example 2) combinations in an array
+
+function functionTask21() {
+    console.log(subset([1, 2, 3], 2));
+}
+
+function subset(arr, len) {
+    var fn = function(n, src, got, all) {
+        if (n == 0) {
+            if (got.length > 0) {
+                all[all.length] = got;
+            }
+            return;
+        }
+        for (var j = 0; j < src.length; j++) {
+            fn(n - 1, src.slice(j + 1), got.concat([src[j]]), all);
+        }
+        return;
+    };
+    var all = [];
+    //for (var i = min; i < arr.length; i++) {
+        fn(len, arr, [], all);
+    //}
+    //all.push(arr);
+    return all;
+}
+
+//алгоритм основан на использовании кодов Грея
+// function subset2(arr, arr_size){
+//     var result_set = [],
+//         result;
+//      var i;
+//     for(var j = 0; j < Math.pow(2, arr.length); j++){ // 2^len комбинаций
+//         result = [];
+//         i = arr.length - 1;
+//         do{
+//             if( (j & (1 << i)) !== 0) {
+//                 result.push(arr[i]);
+//             }
+//         }  while(i--);
+//
+//         if( result.length >= arr_size){ //если поставить == , то будет нужная длина
+//             result_set.push(result);
+//         }
+//     }
+//
+//     return result_set;
+// }
